32ee49e3e5a916e99db3d8b53861ee09ec20990f
carlosivmm <carlosivmm@gmail.com>
2020-10-17 20:56:46 -0500

    Comentarios en Contador Hooks

--- src/app/main/contador/components/ExampleCounterHooks.js
+++ src/app/main/contador/components/ExampleCounterHooks.js
@@ -8,8 +8,10 @@ import useCounter from "../hooks/useCounter";
 
 const ExampleCounterHooks = props => {
   const { initCounter } = props;
+  //DESTRUCTURING PARA ACCEDER A LOS DATOS QUE RETORNA EL HOOK
   const [count, increase, decrease] = useCounter(initCounter);
 
+  //SE RETORNA EL UI CON LAS FUNCIONALIDADES Y VARIABLES DEL HOOK
   return (
     <div className="flex w-full  flex-col">
       <div className="flex justify-center">
--- src/app/main/contador/hooks/useCounter.js
+++ src/app/main/contador/hooks/useCounter.js
@@ -1,11 +1,20 @@
+//MANEJO DE ESTADOS
 import { useState } from 'react';
 
+//VALOR INICIAL DEL CONTADOR
 const useCounter = initCount => {
+	/*
+		SE UTILIZA EL API USESTATE DE REACT
+		PARA EL MANEJO DE ESTADOS
+	*/
 	const [count, setCount] = useState(initCount);
 
+	//MÉTODO PARA INCREMENTAR EL CONTADOR
 	const increase = () => setCount(count + 1);
+	//MÉTODO PARA DECREMENTAR EL CONTADOR
 	const decrease = () => setCount(count - 1);
 
+	//SE RETORNA EN UN ARREGLO LAS FUNCIONALIDADES
 	return [count, increase, decrease];
 };
 
